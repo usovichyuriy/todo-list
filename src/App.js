241475140163c24a150c60fd2eb0d3c3e08eb2bd
yuriy <yuriy.us1919@gmail.com>
2023-02-14 21:55:49 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import Header from './components/Header/Header';
 import './App.css';
 import MenuContainer from './components/Menu/MenuContainer';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <div className='app-wrapper-content'>
-        <MenuContainer />
+    <HashRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path="/"
+              element={<Navigate to="/menu" />} />
+            <Route path="/menu"
+              element={<MenuContainer />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </HashRouter>
   );
 }
 
